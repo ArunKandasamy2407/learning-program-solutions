a358f6d80d9c3c39350db2f69cbbf9c55aa4f394
Arun P K <124854853+ArunKandasamy2407@users.noreply.github.com>
2025-07-03 23:35:21 +0530

    Adding week 3 hands on
    submitting week 3 spring core and maven handson

---
+++ Week3_Spring_core_and_maven/2_Spring_core_and_maven/code/src/main/java/org/example/service/BookService.java
@@ -0,0 +1,17 @@
+package org.example.service;
+
+import org.example.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("Adding book: " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
